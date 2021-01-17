6c67ffac74f5a36256d2f5024509e55f25bcb5d4
milliemcq <milliemcq@live.com>
2021-01-17 17:23:08 +0000

    millie commit

--- tti-explorer-with-changes/notebooks/elim/emukit_new/examples/preferential_batch_bayesian_optimization/pbbo/acquisitions/__init__.py
+++
@@ -1,3 +0,0 @@
-from .expectation_acquisition import *
-from .thompson_sampling_acquisition import *
-from .acquisition_function import *
--- tti-explorer-with-changes/notebooks/elim/emukit_new/multi_fidelity/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-
-from . import kernels  # noqa: F401
-from . import models  # noqa: F401
